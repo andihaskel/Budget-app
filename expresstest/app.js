3f8794780155e7e950b84e0868c3888a03ef109c
Andres Haskel <andihas@gmail.com>
2017-02-15 16:58:18 +0200

    error 'd' gabi

---
+++ expresstest/app.js
@@ -0,0 +1,147 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+// var session = require('express-session');
+
+
+
+var mongoose = require('mongoose');
+
+var connection = mongoose.connect('mongodb://localhost:27017', function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected');
+    }
+});
+
+var index = require('./routes/index');
+var User = require('./routes/users');
+var Objective = require('./routes/objectives');
+var Payment = require('./routes/payments');
+var Category = require('./routes/categories');
+var SavingsAccount = require ('./routes/savingsAccounts');
+
+
+
+  //
+  // var cat1 = new Category ({name:'category de andres'});
+  // cat1.save();
+  // var payment1= new Payment ({
+  //   name: 'vegetales',
+  //   isIncome: true,
+  //   isMonthly: true,
+  //   categoryId:cat1._id,
+  //   amount: 30,
+  // });
+  //   // User.paymentId.push(mongoose.Types.ObjectId(payment1._id));
+  // payment1.save(function(err, Payment) {
+  //   var user1 = new User({
+  //     'userName': 'andi',
+  //     'initialCycleDate': 8,
+  //     'paymentId': mongoose.Types.ObjectId(payment1._id)
+  //   });
+  //   user1.save(function (err,User){
+  //     if(err) {console.log(err)};
+  //   });
+  // });
+
+
+
+
+
+// var ejemplo2 = new SavingsAccount({
+//   balance: 300
+// });
+// ejemplo2.save(function(err, savingsAccount){
+//   var objEjemplo2 = new Objective({
+//       name: 'ejemplo2',
+//       currentAmount: 100,
+//       savingsId: mongoose.Types.ObjectId(savingsAccount.id)
+//   });
+//   objEjemplo2.save();
+//   var user1 = new User({
+//     savingsId: mongoose.Types.ObjectId(savingsAccount.id),
+//     name: 'andi'
+//   });
+//   user1.save();
+// });
+
+
+
+//-------
+//--//
+// var andres = new User ({
+//     userName: 'andres',
+//     objective: mongoose.Types.ObjectId(obj.id)
+// });
+// andres.save();
+//
+// //-----
+// var incomeAndres = new Payment({
+//   isIncome:true,
+//   amount: 12,
+//   currency:'pesos',
+//   userId: mongoose.Types.ObjectId(andres.id)
+//
+// });
+// incomeAndres.save();
+
+
+var app = express();
+var server = http.createServer(app);
+
+// server.listen(3000,'localhost',function(){
+//  server.close(function() {
+//  });
+// });
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(session({secret: 'max', saveUnitialized: false, resave: false }));
+
+app.use('/', index);
+
+//app.use('/categories', categories);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {
+
+  };
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ expresstest/controllers/paymentsController.js
@@ -0,0 +1,155 @@
+var mongoose = require('mongoose');
+var Payment = require ('../routes/payments');
+
+module.exports = {
+
+  getPayments: function(req, res) {
+    mongoose.model('User').findById(req.params.user, function(err,User){
+      mongoose.model('Payment').findById(User.paymentId, function (err,Payment){
+        res.send(Payment);
+      });
+    });
+  },
+
+  getSpecificPayment: function(req, res) {
+    mongoose.model('Payment').findById(req.params.payment, function (err,Payment){
+      if(err){
+        console.log(err);
+      }
+      res.send(Payment);
+    });
+  },
+
+  editPayment: function (req, res) {
+    var query = {"_id": req.params.payment};
+    console.log(query);
+
+    var update = {name: req.body.name, amount: req.body.amount, isMonthly: req.body.isMonthly, categoryId: req.body.categoryId};
+    var options = {new: true};
+Payment.findOneAndUpdate(query, update, options, function(err, Payment) {
+  if (err) {
+    console.log('got an error');
+  }
+  console.log(Payment);
+
+  console.log('edito payment');
+
+});
+
+  },
+
+  delete: function (req,res) {
+        console.log('entro a delete');
+    mongoose.model('Payment').findById(req.params.payment, function(err, Payment){
+        if(err){
+          console.log(err);
+        } else {
+          Payment.remove(function(err){});
+        }
+    })
+
+  },
+
+  store: function (req,res) {
+    mongoose.model('User').findById(req.params.user, function(err, User){
+      console.log(req.body);
+      var payment = new Payment(req.body);
+      payment.userId = User._id;
+      payment.save(function(err){
+        if (err) {
+          console.log(err);
+        }
+      });
+      console.log(payment);
+      res.send(payment);
+    });
+
+  },
+
+  getMonthlyPayments: function(req, res) {
+    var now = new Date();
+    var actualDate = new Date (now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    var initialMonthlyDate = new Date (actualDate.getFullYear(), actualDate.getMonth(), 02);
+    // console.log({$gte: initialMonthlyDate , $lte: actualDate});
+    mongoose.model('User').findById(req.params.user, function(err,User){
+      mongoose.model('Payment').find({
+        'userId': User._id,
+        'paymentDate': { $gte: initialMonthlyDate , $lte: actualDate}
+      }, function(err,Payment){
+
+        res.json(Payment);
+      });
+    });
+  },
+
+
+  getFixedPayments: function(req,res) {
+    mongoose.model('User').findById(req.params.user, function(err,User){
+      mongoose.model('Payment').find({
+        'userId': User._id,
+        'isMonthly': true
+      }, function(err,Payment){
+        console.log(Payment);
+        res.json(Payment);
+      });
+    });
+  },
+
+  getSpecificIncomes: function (req, res) {
+    var initialDate = new Date(req.params.initialDate);
+    var finalDate = new Date(req.params.finalDate);
+    mongoose.model('User').findById(req.params.user, function (err, User){
+      mongoose.model('Payment').find({'userId': User._id,
+      'paymentDate': { $gte: initialDate , $lte: finalDate},
+      'isIncome': true,
+
+    },
+    function(err, Payment) {
+      res.send(Payment);
+    });
+  });
+},
+
+getSpecificExcomes: function (req, res) {
+  var initialDate = new Date(req.params.initialDate);
+  var finalDate = new Date(req.params.finalDate);
+  mongoose.model('User').findById(req.params.user, function (err, User){
+    mongoose.model('Payment').find({'userId': User._id,
+    'paymentDate': { $gte: initialDate , $lte: finalDate},
+    'isIncome': false,
+
+  },
+  function(err, Payment) {
+    res.json(Payment);
+  });
+});
+},
+
+getMonthlyBalance: function(req, res) {
+  var now = new Date();
+  var actualDate = new Date (now.getFullYear(), now.getMonth(), now.getDate() + 1);
+  var initialMonthlyDate = new Date (actualDate.getFullYear(), actualDate.getMonth(), 02);
+  mongoose.model('User').findById(req.params.user, function (err, User){
+    mongoose.model('Payment').find({'userId': User._id,
+    'paymentDate': { $gte: initialMonthlyDate , $lte: actualDate}
+  },
+  function(err, Payment) {
+    var incomePayment = 0;
+    var expensePayment = 0;
+    Payment.forEach(function(payment){
+      if(payment.isIncome){
+        incomePayment += payment.amount;
+      }
+      else {
+        expensePayment += payment.amount;
+      }
+    });
+    var fill = (expensePayment * 100) / incomePayment;
+    var balance = incomePayment - expensePayment;
+    res.json({fill: fill, balance: balance});
+  });
+});
+}
+
+
+}
